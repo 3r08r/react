b29bbb4a30f18eb913b30831f0bcf1b354585ec8
Jan <96497313+3r08r@users.noreply.github.com>
2024-05-19 23:03:46 +0200

    message.tsx is a chiled of  App.tsx

---
+++ src/message.tsx
@@ -0,0 +1,11 @@
+// PascalCase is used for file names and camelCase is used for the component name.
+function Message() {
+    //Jsx : Java Spript XML
+    const name = 'Jan';
+    if (name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>;
+
+}
+
+export default Message;
